a25866a359fa7aad43ebac0613087baf24c98440
rusconn <rusconn3@gmail.com>
2022-10-16 11:39:53 +0000

    perf(test): use `swc/jest` instead of `ts-jest`

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,7 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: "ts-jest",
   testEnvironment: "node",
-  // これがあると速くなる
   transform: {
-    "^.+\\.tsx?$": ["ts-jest", { isolatedModules: true }],
+    "^.+\\.ts$": ["@swc/jest"],
   },
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
