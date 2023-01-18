b4ad576e5737f1d497d33ba638e2dbc3373dc3fb
rusconn <rusconn3@gmail.com>
2023-01-18 04:54:09 +0000

    refactor: refine mapper types

--- src/graphql/types/mappers.ts
+++ src/graphql/types/mappers.ts
@@ -4,15 +4,13 @@ import type * as Scalar from "./scalars";
 import type * as Graph from "./schema";
 
 // Date オブジェクトは自動で toISOString() される？ので許容する
-type Timestamps = {
+type WideTimestamps<T> = Omit<T, "createdAt" | "updatedAt"> & {
   createdAt: Date | Scalar.DateTime;
   updatedAt: Date | Scalar.DateTime;
 };
 
-export type User = Omit<Graph.User, "__typename" | "todos" | "createdAt" | "updatedAt"> &
-  Timestamps;
+export type User = WideTimestamps<Omit<Graph.User, "__typename" | "todos">>;
 
-export type Todo = Omit<Graph.Todo, "__typename" | "user" | "createdAt" | "updatedAt"> &
-  Timestamps & {
-    userId: Prisma.User["id"];
-  };
+export type Todo = WideTimestamps<Omit<Graph.Todo, "__typename" | "user">> & {
+  userId: Prisma.User["id"];
+};
