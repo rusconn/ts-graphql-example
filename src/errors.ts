58b6095ef7cc45bee4d2dbe1481f79ec4693ca0d
rusconn <rusconn3@gmail.com>
2023-01-19 05:34:10 +0000

    refactor: remove unnecessary toJSON() from custom error
    when did it become unnecessary?

--- src/errors.ts
+++ src/errors.ts
@@ -1,11 +1,6 @@
 import ExtensibleCustomError from "extensible-custom-error";
 
 /** アプリで使うエラーのベース */
-export class BaseError extends ExtensibleCustomError {
-  // これが無いと pino のログでエラーオブジェクトが {} に化ける
-  toJSON() {
-    return { name: this.name, stack: this.stack };
-  }
-}
+export class BaseError extends ExtensibleCustomError {}
 
 export class ParseError extends BaseError {}
