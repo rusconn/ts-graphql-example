869c1bd7eaf67d804302b5b0277a839066b763dc
rusconn <rusconn3@gmail.com>
2023-06-11 04:26:58 +0000

    refactor: automate merging typeDefs and merging resolvers

--- src/graphql/resolvers/index.ts
+++
@@ -1,8 +0,0 @@
-import merge from "lodash/merge";
-
-import * as node from "./node";
-import * as scalar from "./scalar";
-import * as todo from "./todo";
-import * as user from "./user";
-
-export const resolvers = merge(node.resolvers, scalar.resolvers, todo.resolvers, user.resolvers);
--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,6 +1,14 @@
+import path from "path";
+
+import { loadFilesSync } from "@graphql-tools/load-files";
+import { mergeTypeDefs, mergeResolvers } from "@graphql-tools/merge";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 
-import { resolvers } from "./resolvers";
-import { typeDefs } from "./typeDefs";
+// TODO: 並行ロードする。Promise を広げるのではなく top-level await を使いたい。
+const typesArray = loadFilesSync(path.join(__dirname, "./typeDefs"));
+const resolversArray = loadFilesSync(path.join(__dirname, "./resolvers"));
+
+const typeDefs = mergeTypeDefs(typesArray);
+const resolvers = mergeResolvers(resolversArray);
 
 export const schema = makeExecutableSchema({ typeDefs, resolvers });
