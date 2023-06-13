e034a9c57ead5c552732c9fae20df605ef2be3ab
rusconn <rusconn3@gmail.com>
2023-06-13 18:22:30 +0900

    chore(db): squash migration files

--- prisma/migrations/20230121064143_add_id_user_id_index_to_todo/migration.sql
+++
@@ -1,2 +0,0 @@
--- CreateIndex
-CREATE INDEX "Todo_id_userId_idx" ON "Todo"("id", "userId");
--- prisma/migrations/20230613091227_squashed/migration.sql
+++ prisma/migrations/20230613091227_squashed/migration.sql
@@ -2,16 +2,16 @@
 CREATE TYPE "TodoStatus" AS ENUM ('DONE', 'PENDING');
 
 -- CreateEnum
-CREATE TYPE "Role" AS ENUM ('ADMIN', 'USER', 'GUEST');
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'USER');
 
 -- CreateTable
 CREATE TABLE "Todo" (
-    "id" VARCHAR(255) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "title" VARCHAR(255) NOT NULL,
-    "description" TEXT NOT NULL DEFAULT E'',
-    "status" "TodoStatus" NOT NULL DEFAULT E'PENDING',
+    "description" TEXT NOT NULL DEFAULT '',
+    "status" "TodoStatus" NOT NULL DEFAULT 'PENDING',
+    "id" VARCHAR(255) NOT NULL,
     "userId" VARCHAR(255) NOT NULL,
 
     CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
@@ -19,12 +19,14 @@ CREATE TABLE "Todo" (
 
 -- CreateTable
 CREATE TABLE "User" (
-    "id" VARCHAR(255) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "name" VARCHAR(255) NOT NULL,
-    "token" VARCHAR(255) NOT NULL,
-    "role" "Role" NOT NULL DEFAULT E'USER',
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "token" VARCHAR(255),
+    "role" "Role" NOT NULL DEFAULT 'USER',
+    "id" VARCHAR(255) NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
@@ -33,10 +35,16 @@ CREATE TABLE "User" (
 CREATE INDEX "Todo_userId_idx" ON "Todo"("userId");
 
 -- CreateIndex
+CREATE INDEX "Todo_id_userId_idx" ON "Todo"("id", "userId");
+
+-- CreateIndex
 CREATE INDEX "Todo_createdAt_id_idx" ON "Todo"("createdAt" DESC, "id");
 
 -- CreateIndex
 CREATE INDEX "Todo_updatedAt_id_idx" ON "Todo"("updatedAt" DESC, "id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_token_key" ON "User"("token");
@@ -49,3 +57,4 @@ CREATE INDEX "User_updatedAt_id_idx" ON "User"("updatedAt" DESC, "id");
 
 -- AddForeignKey
 ALTER TABLE "Todo" ADD CONSTRAINT "Todo_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
