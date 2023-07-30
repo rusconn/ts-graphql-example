694f78bc00ffec10a46a6826ac33fa6f96cfac1f
rusconn <rusconn3@gmail.com>
2023-07-30 18:16:43 +0900

    refactor: use client extensions instead of $use as it's deprecation

--- src/datasources/prisma.ts
+++ src/datasources/prisma.ts
@@ -3,8 +3,7 @@ import { Prisma, PrismaClient } from "@prisma/client";
 import { isProd } from "@/config";
 import * as DataSource from "@/datasources";
 
-/** Node.js 環境下ではモジュールキャッシュにより singleton */
-export const prisma = new PrismaClient({
+const basePrisma = new PrismaClient({
   log: [
     { level: "query", emit: "event" },
     { level: "info", emit: "stdout" },
@@ -13,7 +12,7 @@ export const prisma = new PrismaClient({
   ],
 });
 
-prisma.$on("query", e => {
+basePrisma.$on("query", e => {
   console.log("\x1b[34mprisma:query\u001b[0m", {
     Query: `${e.query.replaceAll('"public".', "")}`,
     Params: isProd ? "***" : e.params,
@@ -21,24 +20,29 @@ prisma.$on("query", e => {
   });
 });
 
-prisma.$use(async (params, next) => {
-  try {
-    return (await next(params)) as unknown;
-  } catch (e) {
-    if (e instanceof Prisma.PrismaClientKnownRequestError) {
-      switch (e.code) {
-        case "P2000":
-          throw new DataSource.InputTooLongError(e);
-        case "P2001":
-        case "P2025":
-          throw new DataSource.NotFoundError(e);
-        case "P2002":
-          throw new DataSource.NotUniqueError(e);
-        default:
-          throw new DataSource.DataSourceError(e);
-      }
-    }
+/** Node.js 環境下ではモジュールキャッシュにより singleton */
+export const prisma = basePrisma.$extends({
+  query: {
+    async $allOperations({ args, query }) {
+      try {
+        return (await query(args)) as unknown;
+      } catch (e) {
+        if (e instanceof Prisma.PrismaClientKnownRequestError) {
+          switch (e.code) {
+            case "P2000":
+              throw new DataSource.InputTooLongError(e);
+            case "P2001":
+            case "P2025":
+              throw new DataSource.NotFoundError(e);
+            case "P2002":
+              throw new DataSource.NotUniqueError(e);
+            default:
+              throw new DataSource.DataSourceError(e);
+          }
+        }
 
-    throw e;
-  }
-});
+        throw e;
+      }
+    },
+  },
+}) as PrismaClient; // インターフェースは変えないので PrismaClient とした
