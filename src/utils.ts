0513555fe33e50fe6b4f47719bc0220b5dfc55b2
rusconn <rusconn3@gmail.com>
2023-08-06 00:40:59 +0900

    refactor: ease introspection query detection method

--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,5 @@
-import { DocumentNode, getOperationAST } from "graphql";
-import { gql } from "graphql-tag";
-
-// https://github.com/justinlevi/typorm-issue/blob/eb92b31a581f963707abdea26c49c9893fdb9a86/src/plugins/apolloServerRequestLogger.ts#L18
-export const isIntrospectionQuery = (query: string | DocumentNode) => {
-  const document = typeof query === "string" ? gql(query) : query;
-  const operation = getOperationAST(document);
-
-  return (
-    operation != null &&
-    operation.selectionSet.selections.every(
-      selection => ((selection as any).name.value as string).startsWith("__") // eslint-disable-line
-    )
-  );
+// より正確そうな方法があるがクエリのパースが必要になる
+// https://github.com/justinlevi/typorm-issue/blob/eb92b31a581f963707abdea26c49c9893fdb9a86/src/plugins/apolloServerRequestLogger.ts#L6
+export const isIntrospectionQuery = (query: string) => {
+  return query.includes("__schema") || query.includes("__type");
 };
