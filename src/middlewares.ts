86b8721c70aed2aea5ef7afad81d78b8f1fe152d
rusconn <rusconn3@gmail.com>
2023-08-12 19:04:33 +0900

    fix: a small fix to error handling middleware

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -10,14 +10,14 @@ const permissionAndErrorMiddleware = shield(permissions, {
   // ログの為に埋め込んだ内部情報はサーバーの設定でレスポンスから除外すること
   fallbackError: (thrown, _parent, _args, _context, _info) => {
     if (thrown instanceof ParseError) {
-      throw new GraphQLError(thrown.message, {
+      return new GraphQLError(thrown.message, {
         originalError: thrown,
         extensions: { code: Graph.ErrorCode.BadUserInput },
       });
     }
 
     if (thrown instanceof DataSource.NotFoundError) {
-      throw new GraphQLError("Not found", {
+      return new GraphQLError("Not found", {
         originalError: thrown,
         extensions: { code: Graph.ErrorCode.NotFound },
       });
