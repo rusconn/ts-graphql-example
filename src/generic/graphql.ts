087073c710b5d644bf1a3bbdde70c41c9e3454fb
rusconn <rusconn3@gmail.com>
2023-08-16 14:38:10 +0900

    fix: avoid misinterpreting queries include __typename as introspection

--- src/generic/graphql.ts
+++ src/generic/graphql.ts
@@ -1,5 +1,5 @@
 // より正確そうな方法があるがクエリのパースが必要になる
 // https://github.com/justinlevi/typorm-issue/blob/eb92b31a581f963707abdea26c49c9893fdb9a86/src/plugins/apolloServerRequestLogger.ts#L6
 export const isIntrospectionQuery = (query: string) => {
-  return query.includes("__schema") || query.includes("__type");
+  return query.includes("__schema");
 };
