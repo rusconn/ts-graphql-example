261880b03774f3ce677a3d6f34d68d3968204847
rusconn <rusconn3@gmail.com>
2023-11-24 01:41:21 +0900

    chore(dev): clean seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,77 +1,78 @@
-import bcrypt from "bcrypt";
-
-import { passwordHashRoundsExponent } from "@/config.ts";
-import { type User, prisma, Role, TodoStatus } from "@/prisma/mod.ts";
+import { prisma, Role, TodoStatus } from "@/prisma/mod.ts";
 
 const main = async () => {
-  const users = await createUsers();
-  await createTodos(users);
-};
-
-const createUsers = async () => {
-  const rawData = [
+  const data = [
     {
       id: "01HFFYQP8GEG9ATV44YH6XNJ1V",
       name: "admin",
       email: "admin@admin.com",
-      rawPassword: "adminadmin",
+      /** raw: adminadmin */
+      password: "$2b$10$4YuHiiiZiodsyu7mx18d/OX7CaLC5uH61XX2nHddWabigsfDh87me",
       token: "01HFFYQP8HTEHXJJ3DSTVPPBC0",
       role: Role.ADMIN,
+      todos: [],
     },
     {
       id: "01HFFYQP8H8628NYKTK2ZCNCBV",
       name: "hoge",
       email: "hoge@hoge.com",
-      rawPassword: "hogehoge",
+      /** raw: hogehoge */
+      password: "$2b$10$RjosB2FTBUCsjBsZm0OmiO3jpWqNmt54ybRybC5C1LnUkERwOSzji",
       token: "01HFFYQP8H9PRG5DKFES044S5D",
       role: Role.USER,
+      todos: [
+        {
+          id: "01HFFZ0ABV8BJPJX50Z2PJ3DR9",
+          title: "hoge todo 1",
+          description: "hoge desc 1",
+        },
+      ],
     },
     {
       id: "01HFFYQP8JMVAJ11XVZXDXVGQR",
       name: "piyo",
       email: "piyo@piyo.com",
-      rawPassword: "piyopiyo",
+      /** raw: piyopiyo */
+      password: "$2b$10$tt1xSvAUjwVuBzxaUi.yMugSpVGmka/XfgxtSamq4Zeei7XOC5RK.",
       token: "01HFFYQP8JW273G541HW4TREQY",
       role: Role.USER,
+      todos: [
+        {
+          id: "01HFFZ0ABVVD5HPAARMZR74PHG",
+          title: "piyo todo 1",
+          description: "piyo desc 1",
+          status: TodoStatus.DONE,
+        },
+        {
+          id: "01HFFZ0ABV43SDH3EGT46AM6P1",
+          title: "piyo todo 2",
+          description: "piyo desc 2",
+        },
+      ],
     },
   ];
 
-  const dataPromises = rawData.map(async ({ rawPassword, ...rest }) => ({
-    ...rest,
-    password: await bcrypt.hash(rawPassword, passwordHashRoundsExponent),
-  }));
+  const upserts = data.map(({ todos, ...rest }) =>
+    prisma.user.upsert({
+      where: { id: rest.id },
+      update: {},
+      create: {
+        ...rest,
+        todos: {
+          create: todos,
+        },
+      },
+    })
+  );
 
-  const data = await Promise.all(dataPromises);
-
-  await prisma.user.createMany({ data });
-
-  return data;
+  return Promise.all(upserts);
 };
 
-const createTodos = async ([_admin, hoge, piyo]: Pick<User, "id">[]) => {
-  const data = [
-    {
-      id: "01HFFZ0ABV8BJPJX50Z2PJ3DR9",
-      title: "hoge todo 1",
-      description: "hoge desc 1",
-      userId: hoge.id,
-    },
-    {
-      id: "01HFFZ0ABVVD5HPAARMZR74PHG",
-      title: "piyo todo 1",
-      description: "piyo desc 1",
-      status: TodoStatus.DONE,
-      userId: piyo.id,
-    },
-    {
-      id: "01HFFZ0ABV43SDH3EGT46AM6P1",
-      title: "piyo todo 2",
-      description: "piyo desc 2",
-      userId: piyo.id,
-    },
-  ];
-
-  await prisma.todo.createMany({ data });
-};
-
-await main();
+try {
+  const users = await main();
+  console.log(users);
+} catch (e) {
+  console.error(e);
+} finally {
+  await prisma.$disconnect();
+}
