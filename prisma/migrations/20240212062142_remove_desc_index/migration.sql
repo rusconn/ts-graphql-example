110dbe02273dc03e86dbe58568decc7d4696e96b
rusconn <rusconn3@gmail.com>
2024-02-12 15:34:07 +0900

    change(db): remove unnecessary desc indexes

---
+++ prisma/migrations/20240212062142_remove_desc_index/migration.sql
@@ -0,0 +1,11 @@
+-- DropIndex
+DROP INDEX "Todo_createdAt_desc_id_desc_idx";
+
+-- DropIndex
+DROP INDEX "Todo_updatedAt_desc_id_desc_idx";
+
+-- DropIndex
+DROP INDEX "User_createdAt_desc_id_desc_idx";
+
+-- DropIndex
+DROP INDEX "User_updatedAt_desc_id_desc_idx";
