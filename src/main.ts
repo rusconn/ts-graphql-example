f76065fa31543f4906c78c40607ed569acca60aa
rusconn <rusconn3@gmail.com>
2024-03-24 13:59:39 +0900

    fix: do graceful shutdown

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
+import process from "node:process";
+
 import { port } from "./config.ts";
+import { prisma } from "./prisma/mod.ts";
 import { server } from "./server.ts";
 
 server.listen(port, () => {
   console.info(`Server is running on http://localhost:${port}/graphql`);
 });
 
+const shutdown = async () => {
+  server.close();
+  await prisma.$disconnect();
+};
+
+// プラットフォームに合わせたシグナルハンドリングが必要
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+
 if (import.meta.hot) {
-  import.meta.hot.on("vite:beforeFullReload", () => {
-    server.close();
-  });
+  import.meta.hot.on("vite:beforeFullReload", shutdown);
 }
