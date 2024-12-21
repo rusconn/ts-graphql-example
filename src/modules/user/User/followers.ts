a9ea8ca978066e297e20361e3a14b3ad893752f3
rusconn <rusconn3@gmail.com>
2024-12-21 21:41:18 +0900

    wip

--- src/modules/user/User/followers.ts
+++ src/modules/user/User/followers.ts
@@ -1,4 +1,7 @@
-import type { UserResolvers } from "../../../schema.ts";
+import { FollowerSortKeys, type UserFollowersArgs, type UserResolvers } from "../../../schema.ts";
+import { getCursorConnection } from "../../common/cursor.ts";
+import { parseCursor, parseErr } from "../../common/parsers.ts";
+import { badUserInputErr, dateByUuid } from "../../common/resolvers.ts";
 import { cursorConnection } from "../../common/typeDefs.ts";
 
 const FIRST_MAX = 50;
@@ -45,4 +48,102 @@ export const typeDef = /* GraphQL */ `
   })}
 `;
 
-export const resolver: UserResolvers["followers"] = (parent, _args, context) => null;
+export const resolver: UserResolvers["followers"] = async (parent, args, context, info) => {
+  const parsed = parseArgs(args);
+
+  if (parsed instanceof Error) {
+    throw badUserInputErr(parsed.message, parsed);
+  }
+
+  const { first, after, last, before, reverse } = parsed;
+
+  const connection = await getCursorConnection(
+    async ({ cursor, limit, backward }) => {
+      const [direction, comp] =
+        reverse === backward //
+          ? (["asc", ">"] as const)
+          : (["desc", "<"] as const);
+
+      const page = await context.db
+        .selectFrom("User")
+        .innerJoin("FollowerFollowee", "User.id", "FollowerFollowee.followeeId")
+        .where("User.id", "=", parent.id)
+        .$if(cursor != null, (qb) =>
+          qb.where(({ eb }) => eb("FollowerFollowee.id", comp, cursor!.id)),
+        )
+        .selectAll("User")
+        .select(["FollowerFollowee.id as ffid"])
+        .orderBy("ffid", direction)
+        .limit(limit)
+        .execute();
+
+      return backward ? page.reverse() : page;
+    },
+    () =>
+      context.db
+        .selectFrom("FollowerFollowee")
+        .where("followeeId", "=", parent.id)
+        .select(({ fn }) => fn.countAll().as("count"))
+        .executeTakeFirstOrThrow()
+        .then((result) => Number(result.count)),
+    { first, after, last, before },
+    {
+      resolveInfo: info,
+      getCursor: (record) => ({ id: record.ffid }),
+      recordToEdge: (record) => ({
+        node: record,
+        followedAt: dateByUuid(record.ffid),
+      }),
+    },
+  );
+
+  if (connection instanceof Error) {
+    throw connection;
+  }
+
+  return connection;
+};
+
+const parseArgs = (args: UserFollowersArgs) => {
+  const { first, after, last, before, ...rest } = args;
+
+  if (first && first > FIRST_MAX) {
+    return parseErr(`"first" must be up to ${FIRST_MAX}`);
+  }
+  if (last && last > LAST_MAX) {
+    return parseErr(`"last" must be up to ${LAST_MAX}`);
+  }
+
+  const parsedAfter = after != null ? parseCursor(after) : null;
+  const parsedBefore = before != null ? parseCursor(before) : null;
+
+  if (parsedAfter instanceof Error) {
+    return parsedAfter;
+  }
+  if (parsedBefore instanceof Error) {
+    return parsedBefore;
+  }
+
+  return { first, after: parsedAfter, last, before: parsedBefore, ...rest };
+};
+
+if (import.meta.vitest) {
+  describe("Parsing", () => {
+    const valids = [{ first: 10 }, { last: 10 }, { first: FIRST_MAX }, { last: LAST_MAX }];
+
+    const invalids = [{ first: FIRST_MAX + 1 }, { last: LAST_MAX + 1 }];
+
+    const reverse = true;
+    const sortKey = FollowerSortKeys.FollowedAt;
+
+    test.each(valids)("valids %#", (args) => {
+      const parsed = parseArgs({ ...args, reverse, sortKey });
+      expect(parsed instanceof Error).toBe(false);
+    });
+
+    test.each(invalids)("invalids %#", (args) => {
+      const parsed = parseArgs({ ...args, reverse, sortKey });
+      expect(parsed instanceof Error).toBe(true);
+    });
+  });
+}
