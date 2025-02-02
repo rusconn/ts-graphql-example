f71a38e048782e84b2dc6dd68bbd5bee5162f37e
rusconn <rusconn3@gmail.com>
2025-02-02 18:20:17 +0900

    refactor: fix parseCursor type

--- src/graphql/_parsers/cursor.ts
+++ src/graphql/_parsers/cursor.ts
@@ -1,7 +1,7 @@
 import { parseErr } from "./util.ts";
 
 export const parseCursor =
-  <T extends string>(isCursor: (x: unknown) => x is T) =>
+  <T>(isCursor: (x: unknown) => x is T) =>
   (input: string) => {
     if (!isCursor(input)) {
       return parseErr(`invalid cursor: ${input}`);
