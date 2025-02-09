fccbda7776729529b0f22eeb787c9dd99039605b
rusconn <rusconn3@gmail.com>
2025-02-09 21:31:40 +0900

    change(log): simplify query error logs

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,5 +1,5 @@
-import { type ErrorLogEvent, Kysely, PostgresDialect } from "kysely";
-import pg from "pg";
+import { Kysely, type LogEvent, PostgresDialect } from "kysely";
+import pg, { type DatabaseError } from "pg";
 
 import { connectionString, isProd } from "../config.ts";
 import { logger } from "../logger.ts";
@@ -10,12 +10,12 @@ pg.types.setTypeParser(pg.types.builtins.INT8, Number);
 
 const [logQuery, logError] = isProd
   ? [
-      (obj: object) => logger.info(obj, "query-info"),
-      (event: ErrorLogEvent) => logger.error(event, "query-error"),
+      (obj: Record<string, unknown>) => logger.info(obj, "query-info"),
+      (obj: Record<string, unknown>) => logger.error(obj, "query-error"),
     ]
   : [
-      (obj: object) => console.log("kysely:query", obj),
-      (event: ErrorLogEvent) => console.error("%o", event),
+      (obj: Record<string, unknown>) => console.log("kysely:query", obj),
+      (obj: Record<string, unknown>) => console.error("kysely:error", obj),
     ];
 
 /** Node.js 環境下ではモジュールキャッシュにより singleton */
@@ -28,15 +28,28 @@ export const client = new Kysely<DB>({
   log(event) {
     switch (event.level) {
       case "query":
-        logQuery({
-          sql: event.query.sql,
-          params: isProd ? "***" : event.query.parameters,
-          duration: `${Math.round(event.queryDurationMillis)}ms`,
+        logQuery(common(event));
+        break;
+      case "error": {
+        const e = event.error as DatabaseError;
+        logError({
+          message: e.message,
+          stack: e.stack,
+          table: e.table,
+          code: e.code,
+          constraint: e.constraint,
+          ...common(event),
         });
         break;
-      case "error":
-        logError(event);
-        break;
+      }
+      default:
+        throw new Error(event satisfies never);
     }
   },
+});
+
+const common = (event: LogEvent) => ({
+  sql: event.query.sql,
+  params: isProd ? "***" : event.query.parameters,
+  duration: `${Math.round(event.queryDurationMillis)}ms`,
 });
