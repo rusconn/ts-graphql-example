36b2df014c9947918f88e72151d0df3047b89593
rusconn <rusconn3@gmail.com>
2025-02-11 21:23:31 +0900

    change!: make query limit stricter

--- src/plugins/armor.ts
+++ src/plugins/armor.ts
@@ -3,6 +3,15 @@ import { EnvelopArmorPlugin } from "@escape.tech/graphql-armor";
 import { maxCost, maxDepth } from "../config/armor.ts";
 
 export const armor = EnvelopArmorPlugin({
-  costLimit: { maxCost },
-  maxDepth: { n: maxDepth },
+  maxDepth: {
+    n: maxDepth,
+    flattenFragments: true,
+  },
+  costLimit: {
+    maxCost,
+    objectCost: 3,
+    scalarCost: 1,
+    depthCostFactor: 2,
+    flattenFragments: true,
+  },
 });
