fe1c353be9445332801352195bf19fd3a87ae0ee
rusconn <rusconn3@gmail.com>
2025-11-25 10:50:03 +0900

    chore(deps): upgrade prisma

--- src/db/types.ts
+++ src/db/types.ts
@@ -5,55 +5,55 @@ export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
 export type Timestamp = ColumnType<Date, Date | string, Date | string>;
 
 export const TodoStatus = {
-  DONE: "DONE",
-  PENDING: "PENDING",
+    DONE: "DONE",
+    PENDING: "PENDING"
 } as const;
-export type TodoStatus = typeof TodoStatus[keyof typeof TodoStatus];
+export type TodoStatus = (typeof TodoStatus)[keyof typeof TodoStatus];
 export const UserRole = {
-  ADMIN: "ADMIN",
-  USER: "USER",
+    ADMIN: "ADMIN",
+    USER: "USER"
 } as const;
-export type UserRole = typeof UserRole[keyof typeof UserRole];
+export type UserRole = (typeof UserRole)[keyof typeof UserRole];
 export type TodoTable = {
-  id: string;
-  updatedAt: Timestamp;
-  title: string;
-  description: Generated<string>;
-  status: Generated<TodoStatus>;
-  userId: string;
+    id: string;
+    updatedAt: Timestamp;
+    title: string;
+    description: Generated<string>;
+    status: Generated<TodoStatus>;
+    userId: string;
 };
 export type Todo = Selectable<TodoTable>;
 export type NewTodo = Insertable<TodoTable>;
 export type TodoUpdate = Updateable<TodoTable>;
 export type UserTable = {
-  id: string;
-  updatedAt: Timestamp;
-  name: string;
-  email: string;
-  role: Generated<UserRole>;
+    id: string;
+    updatedAt: Timestamp;
+    name: string;
+    email: string;
+    role: Generated<UserRole>;
 };
 export type User = Selectable<UserTable>;
 export type NewUser = Insertable<UserTable>;
 export type UserUpdate = Updateable<UserTable>;
 export type UserCredentialTable = {
-  userId: string;
-  updatedAt: Timestamp;
-  password: string;
+    userId: string;
+    updatedAt: Timestamp;
+    password: string;
 };
 export type UserCredential = Selectable<UserCredentialTable>;
 export type NewUserCredential = Insertable<UserCredentialTable>;
 export type UserCredentialUpdate = Updateable<UserCredentialTable>;
 export type UserTokenTable = {
-  userId: string;
-  updatedAt: Timestamp;
-  token: string;
+    userId: string;
+    updatedAt: Timestamp;
+    token: string;
 };
 export type UserToken = Selectable<UserTokenTable>;
 export type NewUserToken = Insertable<UserTokenTable>;
 export type UserTokenUpdate = Updateable<UserTokenTable>;
 export type DB = {
-  Todo: TodoTable;
-  User: UserTable;
-  UserCredential: UserCredentialTable;
-  UserToken: UserTokenTable;
+    Todo: TodoTable;
+    User: UserTable;
+    UserCredential: UserCredentialTable;
+    UserToken: UserTokenTable;
 };
