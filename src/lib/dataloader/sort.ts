1bee72b3f120b0a3ed3057c30c9b04e2f84c4e2f
rusconn <rusconn3@gmail.com>
2026-02-06 19:27:12 +0900

    refactor(lib): clean sort function

--- src/lib/dataloader/sort.ts
+++ src/lib/dataloader/sort.ts
@@ -5,7 +5,5 @@ export const sort = <Key extends string | number | bigint, Value, Default = unde
   defaultValue?: Default,
 ) => {
   const kv = new Map(values.map((value) => [toKey(value), value]));
-  return keys.map((key) => kv.get(key) ?? defaultValue) as Default extends undefined
-    ? (Value | undefined)[]
-    : (Value | Default)[];
+  return keys.map((key) => kv.get(key) ?? (defaultValue as Default));
 };
