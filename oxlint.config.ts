5ae80a62c768257be7844348b926c79ad09b5c99
rusconn <rusconn3@gmail.com>
2026-06-01 08:56:25 +0900

    chore(dev): clean configs for oxlint and oxfmt

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -5,7 +5,6 @@ export default defineConfig({
     "e2e/graphql/_shared/types.ts",
     "src/infrastructure/datasources/_shared/generated.ts",
     "src/presentation/graphql/schema/_types.ts",
-    "pnpm-lock.yaml",
   ],
   categories: {
     correctness: "error",
