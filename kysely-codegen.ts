3b0f180d1dd73fe23a9ea215b7dbf3c5feae71c9
rusconn <rusconn3@gmail.com>
2026-06-12 23:10:45 +0900

    refactor(dev): clean kysely-codegen.ts

--- kysely-codegen.ts
+++ kysely-codegen.ts
@@ -6,7 +6,6 @@ export default defineConfig({
     Uuidv7: "../../../util/uuid/v7.ts",
   },
   dialect: "postgres",
-  excludePattern: "test.*",
   outFile: "src/infrastructure/datasources/_shared/generated.ts",
   typeMapping: {
     uuid: "Uuidv7", // uuidv7にするとstringになってしまうよう
